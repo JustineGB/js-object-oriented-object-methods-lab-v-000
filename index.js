ba7220ec3a86f3891d84f2f79b4396d14f3a5540
Justine B <justinegayton@gmail.com>
2019-03-05 20:03:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function BoardMember (name, email){
+function BoardMember (name, homeState, traing){
   this.name = name;
-  this.email = email;
+  this.homeState = homeState;
+   this.training = training;
   this.sayHello = function(){
   console.log(`Hello everybody, my name is ${this.name}!`);
   }
