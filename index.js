5565d17951aeb582058b994667e686194b431136
Justine B <justinegayton@gmail.com>
2019-03-05 20:04:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function BoardMember (name, homeState, traing){
   this.homeState = homeState;
   this.training = training;
   this.veto = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
-  this.veto = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
-  this.veto = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
-  this.veto = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
+  this.approve = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
+  this.doCharity = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
+  this. = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
   this.veto = function(){console.log(`Hello everybody, my name is ${this.name}!`);}
 
 }
